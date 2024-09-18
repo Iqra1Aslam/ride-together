71bb1845947a8deeab8968fe25e8e09442d82660
LittleStar <manaal2fatima@gmail.com>
2024-09-18 15:09:17 +0500

    jjj

--- src/routes/vehicle.routes.js
+++ src/routes/vehicle.routes.js
@@ -23,4 +23,4 @@ vehicleRouter.route('/publish-ride').post(auth_middleware.check_user_role(['driv
 
 // Add the new route for sending requests
 vehicleRouter.route('/send-request').post(auth_middleware.check_user_role(['driver', 'passenger']), vehicle.send_request);
-vehicleRouter.route('/driver/ride-requests').get(auth_middleware.check_user_role(['driver']), vehicle.fetch_ride_requests);
+vehicleRouter.route('/driver/ride-requests/:driverId').get(auth_middleware.check_user_role(['driver', 'passenger']), vehicle.fetch_ride_requests);
